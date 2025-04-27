9d8aa1893ab453cb9e5ff6d69631c9241088a830
PsychoRover <gtorrent12@gmail.com>
2025-04-27 07:50:21 +0000

    ✨ feat(hw1): use MD5 hash for ticket IDs

--- HW1/src/entry_handler.py
+++ HW1/src/entry_handler.py
@@ -2,7 +2,7 @@
 import json
 import os
 import time
-import uuid
+from hashlib import md5
 
 import boto3
 
@@ -12,7 +12,11 @@ table = ddb.Table(os.getenv("TABLE_NAME"))
 
 def lambda_handler(event, _):
     plate = json.loads(event["body"])["plate"]
-    ticket_id = str(uuid.uuid4())
+    ticket_id = md5((plate).encode()).hexdigest()[:8]
     now = int(time.time())
-    table.put_item(Item={"ticketId": ticket_id, "plate": plate, "entryEpoch": now, "ttlEpoch": now + 86_400})
-    return {"statusCode": 201, "body": json.dumps({"ticketId": ticket_id})}
+    # Should activate local dynamodb for testing
+    # table.put_item(Item={"ticketId": ticket_id, "plate": plate, "entryEpoch": now, "ttlEpoch": now + 86_400})
+    return {
+        "statusCode": 201,
+        "body": json.dumps({"ticketId": ticket_id}),
+    }
