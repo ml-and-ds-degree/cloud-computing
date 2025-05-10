a843c389eab552b4bb364cdf9979abc1697a12e7
PsychoRover <gtorrent12@gmail.com>
2025-05-10 13:44:52 +0000

    🔥 chore(hw1): Remove unused files
    - Deleted errors.py, main.py, models.py, and setup-localstack.sh as they are no longer needed in the project.
    - This cleanup helps streamline the codebase and remove obsolete components.

--- HW1/errors.py
+++
@@ -1,5 +0,0 @@
-from http import HTTPStatus
-
-VEHICLE_ALREADY_PARKED = {HTTPStatus.BAD_REQUEST: {"description": "Vehicle already parked"}}
-
-TICKET_NOT_FOUND = {HTTPStatus.NOT_FOUND: {"description": "Ticket not found"}}
